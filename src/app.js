26a383d56242564405367fb979fe3475a297d613
Eduardo <miltormentas@tutanota.com>
2023-07-18 12:56:02 -0400

    started controller logic

--- src/app.js
+++ src/app.js
@@ -1,38 +1,28 @@
 const express= require('express')
-const path= require('path');
+const path = require('path')
 const app = express();
-const hbs = require('hbs')
 
-
-const public_dir_path= path.join(__dirname, '../views')
-app.use(express.static(public_dir_path));
-const partials_path= path.join(__dirname,'../views/partials' )
-const views_path= path.join(__dirname, '../views/templates')
-hbs.registerPartials(partials_path)
+const hbs = require('hbs');
+const views_path = path.join(__dirname, '../views')
 app.set('views', views_path )
 app.set('view engine','hbs')
-
+app.use(express.urlencoded({ extended: true }));
 app.get('',(req, res)=>{
-    res.send(req.query['products'])
+    res.render('index')
 })
+app.post('/test',(req, res)=>{
 
-
-
-app.get('*',(req, res)=>{
-    res.render('error_page');
+  if(req.body.talent==='option1'){
+      res.render('result', {
+          name:'nadal'
+      })
+  }else{
+      res.render('result', {
+          name:'federer'
+      })
+  }
+    // res.send(req.body.talent);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log('server up on port 3000')
+app.listen(5000, ()=>{
+    console.log('app running on port 3000')
 })
